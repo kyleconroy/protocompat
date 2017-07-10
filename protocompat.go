a3bf7e07ff4411d64e36a8c3e33682b99436f2bf
Kyle Conroy <kyle@kyleconroy.com>
2017-07-09 18:25:57 -0700

    Add two different types of compatibility tests

---
+++ protocompat.go
@@ -0,0 +1,28 @@
+package protocompat
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func check(a, b proto.Message) error {
+	data, err := proto.Marshal(a)
+	// Something is serioualy wrong if we can't marshal the message
+	if err != nil {
+		panic(err)
+	}
+	return proto.Unmarshal(data, b)
+}
+
+func AssertCompatible(t *testing.T, a, b proto.Message) {
+	if err := check(a, b); err != nil {
+		t.Errorf("expected messages to be compatible; %s", err)
+	}
+}
+
+func AssertIncompatible(t *testing.T, a, b proto.Message) {
+	if err := check(a, b); err == nil {
+		t.Error("expected messages to be incompatible, but no error")
+	}
+}
